fb781cb9434ba868843ef1fcd81e679ab9d0dbc1
tentacode <contact@gabrielpillet.com>
2016-11-11 13:38:38 +0100

    PauseController 🙌

---
+++ Assets/Scipts/Game/GameController.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+	void Awake ()
+    {
+        SceneManager.LoadScene("PauseMenu", LoadSceneMode.Additive);
+	}
+}
---
+++ Assets/Scipts/Game/PauseController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseController : MonoBehaviour
+{
+    public GameObject pauseCanvas;
+    private bool isPaused = false;
+
+	void Start ()
+    {
+        pauseCanvas.SetActive (false);    
+	}
+	
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            TogglePause ();
+        }
+	}
+
+    void TogglePause()
+    {
+        isPaused = !isPaused;
+        if (isPaused) {
+            pauseCanvas.SetActive(true);
+            Time.timeScale = 0;
+        } else {
+            pauseCanvas.SetActive(false);
+            Time.timeScale = 1;
+        }
+
+    }
+}
