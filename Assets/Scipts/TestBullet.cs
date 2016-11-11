bba043f238f00296fc1b9e47dc7f5cc3f6316e24
Yendhi <dreammakerscircus@gmail.com>
2016-11-11 11:03:04 +0100

    Hey les gars Let's get it Started

---
+++ Assets/Scipts/TestBullet.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestBullet : MonoBehaviour {
+
+
+    private Rigidbody rgbd;
+    private GameObject bullet;
+
+    // Use this for initialization
+    void Start () {
+        rgbd = gameObject.GetComponent<Rigidbody>();
+        bullet = GameObject.Find("lazer");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetMouseButton(1))
+        {
+            Instantiate(bullet);
+        }
+
+    }
+}
