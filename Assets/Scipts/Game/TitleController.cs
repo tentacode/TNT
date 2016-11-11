ff2a16b8447dee50bc81e115b5df2d21a41b9a63
tentacode <contact@gabrielpillet.com>
2016-11-11 14:13:23 +0100

    Title screen 😎

---
+++ Assets/Scipts/Game/TitleController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class TitleController : MonoBehaviour
+{
+	void Update ()
+    {
+        if (Input.GetKeyDown (KeyCode.Space)) {
+            SceneManager.LoadScene("Main");
+        }
+
+        if (Input.GetKeyDown (KeyCode.Escape)) {
+            Application.Quit ();
+        }
+	}
+}
