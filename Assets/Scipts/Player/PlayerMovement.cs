9512a89ca5b8457d45d26e0461aaa99c1465bf99
tentacode <contact@gabrielpillet.com>
2016-11-11 22:39:28 +0100

    Fixing inputs for each players and Mac hack

--- Assets/Scipts/Player/PlayerMovement.cs
+++ Assets/Scipts/Player/PlayerMovement.cs
@@ -6,38 +6,40 @@ public class PlayerMovement : MonoBehaviour {
     public int speed = 10;
     public float turnSpeed = 50f;
 
-    // Update is called once per frame
-    /*void Update () {
-	
-       
-	}*/
-   new Rigidbody rigidbody;
+    private int playerIndex;
+    private Rigidbody rigidbody;
 
     void Start()
     {
+        playerIndex = GetComponent<PlayerIdentity> ().playerIndex;
         rigidbody = gameObject.GetComponent<Rigidbody>();
     }
+
     void FixedUpdate()
     {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
-        float rotateX = Input.GetAxis("RotateX");
-        float rotateY = Input.GetAxis("RotateY");
+        float moveHorizontal = Input.GetAxis(GetPlayerAxis("Horizontal"));
+        float moveVertical = Input.GetAxis(GetPlayerAxis("Vertical"));
+        float rotateX = Input.GetAxis(GetPlayerAxis("RotateX"));
+        float rotateY = Input.GetAxis(GetPlayerAxis("RotateY"));
 
         Vector3 mouvment = new Vector3(moveHorizontal, moveVertical, 0);
         rigidbody.MovePosition(transform.position + mouvment * speed * Time.deltaTime);
 
-
-        ///////////////// rotate player for shoot  //////////////
-     /*   if (Input.GetKey(KeyCode.LeftArrow))
-            transform.Rotate(Vector3.up, -turnSpeed * Time.deltaTime);
-
-        if (Input.GetKey(KeyCode.RightArrow))
-            transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime);*/
-        ////////////////////////////////////////////////////////
-
-
         float angle = Mathf.Atan2(rotateX, rotateY) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler(new Vector3(0, 0, -angle));
+    }
+
+    string GetPlayerAxis(string axis)
+    {
+        #if UNITY_STANDALONE_OSX
+        if (playerIndex == 1 && axis == "RotateX") {
+            axis = "RotateXMac";
+        }
+        if (playerIndex == 1 && axis == "RotateY") {
+            axis = "RotateYMac";
+        }
+        #endif
+
+        return string.Format ("{0}{1}", axis, playerIndex);
     }
 }
--- Assets/Scipts/Player/PlayerShooter.cs
+++ Assets/Scipts/Player/PlayerShooter.cs
@@ -7,17 +7,31 @@ public class PlayerShooter : MonoBehaviour
     public GameObject lazerPrefab;
 
     private Sprite lazerColor;
+    private int playerIndex;
 
 	void Start ()
     {
+        playerIndex = GetComponent<PlayerIdentity> ().playerIndex;
         lazerColor = GetComponent<PlayerIdentity> ().lazerColor;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetButtonDown("Fire1")) {
+        if (IsFiring()) {
             GameObject lazer = (GameObject)Instantiate (lazerPrefab, spawnPoint.position, spawnPoint.rotation);
             lazer.transform.GetChild(0).GetComponent<SpriteRenderer>().sprite = lazerColor;
         }
 	}
+
+    bool IsFiring()
+    {
+        string fireButton = string.Format ("Shoot{0}", playerIndex);
+        #if UNITY_STANDALONE_OSX
+        if (playerIndex == 1) {
+            fireButton = "ShootMac1";
+        }
+        #endif
+
+        return Input.GetButtonDown (fireButton);
+    }
 }
