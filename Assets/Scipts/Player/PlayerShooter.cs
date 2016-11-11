27f4e0a93fae28e045b97cf6d1b9e1519efb457f
tentacode <contact@gabrielpillet.com>
2016-11-11 20:58:27 +0100

    4 players and lazer colors 🌈

--- Assets/Scipts/Player/PlayerShooter.cs
+++ Assets/Scipts/Player/PlayerShooter.cs
@@ -6,15 +6,18 @@ public class PlayerShooter : MonoBehaviour
     public Transform spawnPoint;
     public GameObject lazerPrefab;
 
+    private Sprite lazerColor;
+
 	void Start ()
     {
-	
+        lazerColor = GetComponent<PlayerIdentity> ().lazerColor;
 	}
 	
 	// Update is called once per frame
 	void Update () {
         if (Input.GetButtonDown("Fire1")) {
             GameObject lazer = (GameObject)Instantiate (lazerPrefab, spawnPoint.position, spawnPoint.rotation);
+            lazer.transform.GetChild(0).GetComponent<SpriteRenderer>().sprite = lazerColor;
         }
 	}
 }
