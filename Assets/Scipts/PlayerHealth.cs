cb7ba1023a37a691b1d013e6821e819b2863a7c3
BembaD <bemba.dansoko@laposte.net>
2016-11-11 17:56:43 +0100

    newPlayerHealth

--- Assets/Scipts/PlayerHealth.cs
+++ Assets/Scipts/PlayerHealth.cs
@@ -3,11 +3,14 @@ using System.Collections;
 
 public class PlayerHealth : MonoBehaviour {
  
-enum state
-    {
-        alive, dead
-    }
+enum state{ alive, dead}
+enum mode { play,pause} 
+    int score=0;
+    bool healthPlayer= state.alive;
+    bool gameStatus = mode.play;
+    
 
-    int score;
+   
+
 
 }
