20be8c02b661e6ab6adee24faee965d5586e52c9
BembaD <bemba.dansoko@laposte.net>
2016-11-12 12:35:48 +0100

    health and destroy

--- Assets/Scipts/PlayerHealth.cs
+++ Assets/Scipts/PlayerHealth.cs
@@ -3,17 +3,42 @@ using System.Collections;
 
 public class PlayerHealth : MonoBehaviour {
  
-enum state{ alive, dead}
-enum mode { introduction,countdownBegin,menu,play,pause,endGame} 
-    int score=0;
-  
+    enum state{ alive, dead}
+    enum mode { introduction,countdownBegin,menu,play,pause,endGame} 
+    int score;
+    state etat;
+    public int life;
    
-    void start()
+    void Start()
     {
-        
+        etat = state.alive;
+        score = 0;
+        life = 1;
+    }
+      void Update()
+    {
+        if (life<=0)
+        {
+            ImDead();
+            Debug.Log("gigi");
+        }
+        Debug.Log(gameObject.layer);
     }
 
-   
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer ==9 )
+        {
+            life --;
+            Destroy(other.gameObject);
+        }
+       
+    }
 
+    public void ImDead()
+    {
+        etat = state.dead;
+        Destroy(gameObject);
 
+    }
 }
