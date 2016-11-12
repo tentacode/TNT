9aa29b9690252a1f1b7b6a967cc5753b66b562c6
Yendhi <dreammakerscircus@gmail.com>
2016-11-12 15:33:11 +0100

    game controler

--- Assets/Scipts/Game/GameController.cs
+++ Assets/Scipts/Game/GameController.cs
@@ -7,8 +7,20 @@ public class GameController : MonoBehaviour
     enum gameState {INTRODUCTION,VALIDPLAYERS,INTROGAME,GAME,ENDGAME}
     gameState currentState;
     gameState previousState;
+    
 	void Awake ()
     {
         SceneManager.LoadScene("PauseMenu", LoadSceneMode.Additive);
 	}
+}
+
+public class score
+{
+    string playerName;
+    int playerScore;
+
+    public score(int playerIndex)
+    {
+        player
+    }
 }
