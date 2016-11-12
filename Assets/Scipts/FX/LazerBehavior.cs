c01f6f50865797208dcae343e73929b5b2af5c5a
tentacode <contact@gabrielpillet.com>
2016-11-12 15:56:09 +0100

    Scale lazer

--- Assets/Scipts/FX/LazerBehavior.cs
+++ Assets/Scipts/FX/LazerBehavior.cs
@@ -9,12 +9,17 @@ public class LazerBehavior : MonoBehaviour {
     private Rigidbody rgbd;
     private int bounceCounter;
     private bool hasCollided = false;
+    private bool isExpanding = true;
     public int playerIndex;
+    private Vector3 expandedScale;
+    private Vector3 initialScale;
 
 
     // Use this for initialization
     void Start()
     {
+        initialScale = new Vector3 (0.1f, 1.0f, 0.1f);
+        expandedScale = new Vector3 (0.1f, 1.0f, 1.0f);
         rgbd = GetComponent<Rigidbody> ();
         rgbd.AddForce (transform.forward * lazerSpeed, ForceMode.Impulse); 
 
@@ -25,9 +30,23 @@ public class LazerBehavior : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
+        var child = transform.GetChild (0);
+        if (isExpanding) {
+            child.transform.localScale = Vector3.Lerp (
+                transform.localScale, 
+                expandedScale, 50 * Time.deltaTime
+            );
+
+            if (child.transform.localScale == expandedScale) {
+                isExpanding = false;
+            }
+        }
+
         if (hasCollided) {
+            child.transform.localScale = initialScale;
+            isExpanding = true;
             float angle = Mathf.Atan2 (rgbd.velocity.x, rgbd.velocity.z) * Mathf.Rad2Deg;
-            transform.GetChild (0).transform.rotation = Quaternion.AngleAxis (angle, Vector3.up);
+            child.transform.rotation = Quaternion.AngleAxis (angle, Vector3.up);
             hasCollided = false;
         }
 
