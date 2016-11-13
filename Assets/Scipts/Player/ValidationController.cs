819f415c5db8cfa135cc7dc97a79eb5fef196de4
tentacode <contact@gabrielpillet.com>
2016-11-13 11:33:45 +0100

    Select screen

--- Assets/Scipts/Player/ValidationController.cs
+++ Assets/Scipts/Player/ValidationController.cs
@@ -1,11 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class ValidationController : MonoBehaviour {
 
     public GameObject image1,image2,image3,image4,text1,press1,press2,press3,press4;
     int countPlayer;
-    bool img1, img2, img3, img4, start;
+    bool playerReady1, playerReady2, playerReady3, playerReady4, start;
+    bool gamepadReady1, gamepadReady2, gamepadReady3, gamepadReady4;
 	// Use this for initialization
 	void Start () {
         //image du player
@@ -16,81 +18,140 @@ public class ValidationController : MonoBehaviour {
         // texte start pour commencer
         text1.SetActive(false);
 
-        //le texte press button 
+        //le texte press button
         press1.SetActive(true);
         press2.SetActive(true);
         press3.SetActive(true);
         press4.SetActive(true);
 
         countPlayer = 0;
-        img1 = false;
-        img2 = false;
-        img3 = false;
-        img4 = false;
+        playerReady1 = false;
+        playerReady2 = false;
+        playerReady3 = false;
+        playerReady4 = false;
         start = false;
     }
 
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetButtonDown("Shoot1"))
-        {
-            press1.SetActive(false);
-            image1.SetActive(true);
-            if (img1==false)
-            {
-                countPlayer += 1;
-                img1 = true;
-            }
-            Debug.Log(countPlayer);
-        }
-        if (Input.GetButtonDown("Shoot2"))
-        {
-            press2.SetActive(false);
-            image2.SetActive(true);
-            
-            if (img2 == false)
-            {
-                countPlayer += 1;
-                img2 = true;
-            }
-            Debug.Log(countPlayer);
-        }
-        if (Input.GetButtonDown("Shoot3"))
-        {
-            press3.SetActive(false);
-            image3.SetActive(true);
-            if (img3 == false)
-            {
-                countPlayer += 1;
-                img3= true;
-            }
-        }
-        if (Input.GetButtonDown("Shoot4"))
-        {
-            press4.SetActive(false);
-            image4.SetActive(true);
-            if (img4 == false)
-            {
-                countPlayer += 1;
-                img4 = true;
-            }
-        }
+        CheckPlayer1();
+        CheckPlayer2();
+        CheckPlayer3();
+        CheckPlayer4();
 
         if (countPlayer>=2)
         {
             text1.SetActive(true);
-            if (start == false)
-            {
-                start = true;
-            }
+            start = true;
         }
-        if (start == true)
+
+        if (start && Input.GetButtonDown("Submit"))
         {
-            if (Input.GetButtonDown("Submit"))
-            {
+            SceneManager.LoadScene("Main");
+        }
+    }
 
-            }
+    int GetPlayerIndexButton(int buttonIndex)
+    {
+        #if UNITY_STANDALONE_OSX
+        buttonIndex += 16;
+        #endif
+
+        int playerIndex = 0;
+        if (!gamepadReady1 && Input.GetKeyDown (string.Format("joystick 1 button {0}", buttonIndex))) {
+            playerIndex = 1;
+            gamepadReady1 = true;
+        } else if (!gamepadReady2 && Input.GetKeyDown (string.Format("joystick 2 button {0}", buttonIndex))) {
+            playerIndex = 2;
+            gamepadReady2 = true;
+        } else if (!gamepadReady3 && Input.GetKeyDown (string.Format("joystick 3 button {0}", buttonIndex))) {
+            playerIndex = 3;
+            gamepadReady3 = true;
+        } else if (!gamepadReady4 && Input.GetKeyDown (string.Format("joystick 4 button {0}", buttonIndex))) {
+            playerIndex = 4;
+            gamepadReady4 = true;
+        } 
+
+        return playerIndex;
+    }
+
+    void CheckPlayer1()
+    {
+        if (playerReady1) {
+            return;
         }
 
+        int playerIndex = GetPlayerIndexButton(0);
+        if (playerIndex == 0) {
+            return;
+        }
+
+        press1.SetActive (false);
+        image1.SetActive (true);
+
+        countPlayer += 1;
+        playerReady1 = true;
+
+        PlayerPrefs.SetInt ("Player1", playerIndex);
+    }
+
+    void CheckPlayer2()
+    {
+        if (playerReady2) {
+            return;
+        }
+
+        int playerIndex = GetPlayerIndexButton(1);
+        if (playerIndex == 0) {
+            return;
+        }
+
+        press2.SetActive (false);
+        image2.SetActive (true);
+
+        countPlayer += 1;
+        playerReady2 = true;
+
+        PlayerPrefs.SetInt ("Player2", playerIndex);
+    }
+
+    void CheckPlayer3()
+    {
+        if (playerReady3) {
+            return;
+        }
+
+        int playerIndex = GetPlayerIndexButton(2);
+        if (playerIndex == 0) {
+            return;
+        }
+
+        press3.SetActive (false);
+        image3.SetActive (true);
+
+        countPlayer += 1;
+        playerReady3 = true;
+
+        PlayerPrefs.SetInt ("Player3", playerIndex);
+    }
+
+    void CheckPlayer4()
+    {
+        if (playerReady4) {
+            return;
+        }
+
+        int playerIndex = GetPlayerIndexButton(3);
+        if (playerIndex == 0) {
+            return;
+        }
+
+        press4.SetActive (false);
+        image4.SetActive (true);
+
+        countPlayer += 1;
+        playerReady4 = true;
+
+        PlayerPrefs.SetInt ("Player4", playerIndex);
     }
 }
